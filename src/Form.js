a982755f3e178d92e5b0f46528c13d6e328a6f19
Tashby <tashby2714@gmail.com>
2020-05-17 20:40:28 -0400

    home and form rendering

---
+++ src/Form.js
@@ -0,0 +1,4 @@
+294 lines (272 sloc) 7.98 KB
+import React, {useState} from "react";
+import * as yup from "yup";
+import axios from "axios";
---
+++ src/HomePage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function HomePage() {
+    return (
+        <div> 
+            <div>
+                <nav>
+                    <a href="#">Home</a>
+                    <a href="#">Help</a>
+                </nav>
+                <br></br>
+                <h2> Pizza </h2>
+            </div>
+        </div>
+    )
+}
