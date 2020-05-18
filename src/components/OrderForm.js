8bb32cdeeb4e95c1650841b5ab27e1ec273cf768
Tashby <tashby2714@gmail.com>
2020-05-17 22:48:38 -0400

    mvp

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -1,79 +1,171 @@
 import React, {useState} from "react";
+import * as yup from "yup";
 import axios from "axios";
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 
-export default function OrderForm() {
-    return (
+//Yup Validation
+const formSchema = yup.object().shape({
+    name: yup
+        .string()
+        .min(2, "Name must be at least 2 characters")
+        .required("Name is a required field"),
+    pizzaSize: yup
+        .string()
+        .required("Must Select a Pizza Size"),
+    sausage: yup
+        .string(),
+    pepperoni: yup
+        .string(),
+    bacon: yup
+        .string(),
+    veggies: yup
+        .string(),
+    special: yup
+        .string()
+})
 
-            <Form>
+//Set state
+export default function OrderForm() {
+    const [formState, setFormState] = useState({
+        name: "",
+        pizzaSize: "",
+        sausage: false,
+        pepperoni: false,
+        bacon: false,
+        veggies: false,
+        special: ""
+    });
+
+    const [post, setPost] = useState()
+
+    const [errorState, setErrorState] = useState({
+        name: "",
+        pizzaSize: "",
+        sausage: "",
+        pepperoni: "",
+        bacon: "",
+        veggies: "",
+        special: ""
+    });
+
+    const validate = e => {
+        let value = 
+        e.target.type ==="checkbox" ? e.target.checked : e.target.value;
+        yup
+            .reach(formSchema, e.target.name)
+            .validate(value)
+            .then(valid => {
+                setErrorState({
+                    ...errorState, [e.target.name]: ""
+                });
+            }) 
+            .catch(err => {
+                console.log(err.errors)
+                setErrorState({
+                    ...errorState,
+                    [e.target.name] : err.errors[0]
+                });
+            });
+    };
+
+    const inputChange = e => {
+        e.persist();
+
+        validate(e)
+        let value =
+        e.target.type === "checkbox" ? e.target.checked : e.target.value;
+        setFormState({...formState, [e.target.name]: value});
+    };
+
+    const formSubmit = e => {
+        e.preventDefault();
+        console.log("form submitted!")
+        
+        axios
+            .post("https://reqres.in/api/users", formState)
+            .then(response => { 
+                  setPost(response.data);
+                  console.log("Success", response)
+                })
+            .catch(err => console.log("Error! Resubmit your form please!",err));
+    setFormState({name: "", pizzaSize: "", sausage: false, pepperoni: false, bacon:false, veggies:false, special: ""})
+    };
+
+    return (
+        //Form Start
+      <Form onSubmit={formSubmit}>
+
+
       <FormGroup>
-        <Label for="exampleEmail">Email</Label>
-        <Input type="email" name="email" id="exampleEmail" placeholder="with a placeholder" />
+        <Label for="nam">Name:</Label>
+        <Input  type="text"
+                    name="name"
+                    id="name"
+                    value={formState.name}
+                    onChange={inputChange} />
+                    
       </FormGroup>
+
+
       <FormGroup>
-        <Label for="examplePassword">Password</Label>
-        <Input type="password" name="password" id="examplePassword" placeholder="password placeholder" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleSelect">Select</Label>
-        <Input type="select" name="select" id="exampleSelect">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
+        <Label for="pizzaSize">Select Pizza</Label>
+        <Input type="select"  
+                    value={formState.pizzaSize}
+                    name="pizzaSize"
+                    id="pizzaSize"
+                    onChange={inputChange}>
+          <option value="" disabled={true}>Please Select Pizza Size</option>
+                    <option value="Small">Small</option>
+                    <option value="Medium">Medium</option>
+                    <option value="Large">Large</option>
         </Input>
       </FormGroup>
-      <FormGroup>
-        <Label for="exampleSelectMulti">Select Multiple</Label>
-        <Input type="select" name="selectMulti" id="exampleSelectMulti" multiple>
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </Input>
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleText">Text Area</Label>
-        <Input type="textarea" name="text" id="exampleText" />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleFile">File</Label>
-        <Input type="file" name="file" id="exampleFile" />
-        <FormText color="muted">
-          This is some placeholder block-level help text for the above input.
-          It's a bit lighter and easily wraps to a new line.
-        </FormText>
-      </FormGroup>
-      <FormGroup tag="fieldset">
-        <legend>Radio Buttons</legend>
-        <FormGroup check>
-          <Label check>
-            <Input type="radio" name="radio1" />{' '}
-            Option one is this and that—be sure to include why it's great
-          </Label>
-        </FormGroup>
-        <FormGroup check>
-          <Label check>
-            <Input type="radio" name="radio1" />{' '}
-            Option two can be something else and selecting it will deselect option one
-          </Label>
-        </FormGroup>
-        <FormGroup check disabled>
-          <Label check>
-            <Input type="radio" name="radio1" disabled />{' '}
-            Option three is disabled
-          </Label>
-        </FormGroup>
-      </FormGroup>
+    
+      
       <FormGroup check>
         <Label check>
-          <Input type="checkbox" />{' '}
-          Check me out
+          <Input  name="pepperoni"
+                    type="checkbox"
+                    checked={formState.pepperoni}
+                    onChange={inputChange} />{' '}
+          pepperoni
+        </Label>
+        <Label check>
+          <Input  name="bacon"
+                    type="checkbox"
+                    checked={formState.bacon}
+                    onChange={inputChange} />{' '}
+          bacon
+        </Label>
+        <Label check>
+          <Input  name="sausage"
+                    type="checkbox"
+                    checked={formState.sausage}
+                    onChange={inputChange} />{' '}
+          Sausage
+        </Label>
+        <Label check>
+          <Input  name="veggies"
+                    type="checkbox"
+                    checked={formState.veggies}
+                    onChange={inputChange} />{' '}
+          veggies
         </Label>
       </FormGroup>
+
+      <FormGroup>
+        <Label for="exampleText">Special Instructions:</Label>
+        <Input type="textarea" 
+        id="special"
+                    name="special"
+                    value={formState.special}
+                    onChange={inputChange} />
+      </FormGroup>
+     
+
       <Button>Submit</Button>
+
+
     </Form>
 
     )
